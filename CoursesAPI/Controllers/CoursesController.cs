b089aadcb6a1bba8afc79f86c31d46f627325f80
Kristinn Heiðar Freysteinsson <krihfre@hotmail.com>
2016-10-12 19:19:33 +0000

    added second project along with some minor changes

---
+++ CoursesAPI/Controllers/CoursesController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using A06.CoursesAPI.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace A06.CoursesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    public class CoursesController : Controller
+    {
+        private static List<Course> _courses = new List<Course>
+        {
+            new Course
+            {
+                Id = 1,
+                Name = "Vefþjónustur",
+                Semester = "20163"
+            }
+        };
+
+        // GET api/courses
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult GetCourses()
+        {
+            return Ok(_courses);
+        }
+
+        // POST api/courses
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // GET api/courses/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+    }
+}
---
+++ CoursesAuth/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace A06.CoursesAuth
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDeveloperIdentityServer();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(LogLevel.Debug);
+            app.UseDeveloperExceptionPage();
+
+            app.UseIdentityServer();
+        }
+    }
+}
